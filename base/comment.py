d1dbc0c67b2f55d04e26fcf8a81de3fcf550da88
Mike L <mike.leeder@gmail.com>
2024-06-06 15:58:16 -0700

    add pydantic

--- base/comment.py
+++ base/comment.py
@@ -1,8 +1,8 @@
 from datetime import datetime
+from pydantic import BaseModel
 
-class Comment():
+class Comment(BaseModel):
     name: str
     text: str
     date: datetime
     vote_count: int
-    
